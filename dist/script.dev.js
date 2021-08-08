5a1159951535393103491f26f146dbfda0d9ce79
opheus2 <j.ominiabohs@gmail.com>
2021-08-08 17:25:16 -0400

    update

---
+++ dist/script.dev.js
@@ -0,0 +1,81 @@
+"use strict";
+
+var map = document.querySelector("#map");
+var searchInput = document.querySelector(".searchInput");
+var searchActionElement = document.querySelector("#search");
+var ipaddressElement = document.querySelector("#ipaddress");
+var locationElement = document.querySelector("#location");
+var timezoneElement = document.querySelector("#timezone");
+var ispElement = document.querySelector("#isp"); // const ipApiKey = "8c22a9655fcffc4fd221092a1c0b0f7a76747ad2c5ac1ab0f2ff74ed"
+
+var ipApiKey = 'at_RAHs0H4kxTidkUnjr1AxMFj3Uq18S';
+var geoIpUrl = "https://geo.ipify.org/api/v1?apiKey=".concat(ipApiKey);
+var mapApiKey = "pk.eyJ1Ijoib3BoZXVzIiwiYSI6ImNpeGF5NjN1YzAwNmMyb29jMG4xenE0dnIifQ.DsyYT7XaF6qgKcB5kphFJQ";
+var ipData;
+
+function json(url) {
+  return regeneratorRuntime.async(function json$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+          return _context.abrupt("return", fetch(url).then(function (res) {
+            return res.json();
+          }));
+
+        case 1:
+        case "end":
+          return _context.stop();
+      }
+    }
+  });
+}
+
+function setIpData(ipaddress) {
+  json("".concat(geoIpUrl, "&ipAddress=").concat(ipaddress ? ipaddress : "") // `https://api.ipdata.co/${ipaddress ? ipaddress : ""}?api-key=${ipApiKey}`
+  ).then(function (data) {
+    console.log(data);
+    ipData = data;
+
+    if (ipData) {
+      ipaddressElement.innerHTML = ipData.ip;
+      locationElement.innerHTML = "".concat(ipData.location.city ? ipData.location.city : "", ", ").concat(ipData.location.region ? ipData.location.region : "", " ").concat(ipData.location.postalCode ? ipData.location.postalCode : "");
+      timezoneElement.innerHTML = "UTC ".concat(ipData.location.timezone);
+      ispElement.innerHTML = ipData.isp; // L.latng
+
+      var lat = ipData.location.lat;
+      var lng = ipData.location.lng;
+      marker.setLatLng([lat, lng]);
+      mymap.setView([lat, lng], 13);
+    }
+  });
+}
+
+function validateIpAddress(ipaddress) {
+  if (/^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|$)){4}$/.test(ipaddress)) {
+    return true;
+  }
+
+  return false;
+}
+
+searchActionElement.addEventListener("click", function (e) {
+  if (validateIpAddress(searchInput.value)) {
+    setIpData(searchInput.value);
+  }
+});
+var mymap = L.map("map").setView([51.505, -0.09], 13);
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: "mapbox/streets-v11",
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken: mapApiKey
+}).addTo(mymap);
+var myIcon = L.icon({
+  iconUrl: "images/icon-location.svg"
+});
+var marker = L.marker([51.505, -0.09], {
+  icon: myIcon
+}).addTo(mymap);
+setIpData();
---
+++ script.js
@@ -0,0 +1,79 @@
+const map = document.querySelector("#map")
+const searchInput = document.querySelector(".searchInput")
+const searchActionElement = document.querySelector("#search")
+const ipaddressElement = document.querySelector("#ipaddress")
+const locationElement = document.querySelector("#location")
+const timezoneElement = document.querySelector("#timezone")
+const ispElement = document.querySelector("#isp")
+
+// const ipApiKey = "8c22a9655fcffc4fd221092a1c0b0f7a76747ad2c5ac1ab0f2ff74ed"
+const ipApiKey = 'at_RAHs0H4kxTidkUnjr1AxMFj3Uq18S'
+const geoIpUrl = `https://geo.ipify.org/api/v1?apiKey=${ipApiKey}`
+
+const mapApiKey =
+    "pk.eyJ1Ijoib3BoZXVzIiwiYSI6ImNpeGF5NjN1YzAwNmMyb29jMG4xenE0dnIifQ.DsyYT7XaF6qgKcB5kphFJQ"
+
+let ipData;
+
+async function json(url) {
+    return fetch(url).then((res) => res.json());
+}
+
+function setIpData(ipaddress) {
+    json(
+        `${geoIpUrl}&ipAddress=${ipaddress ? ipaddress : ""}`
+        // `https://api.ipdata.co/${ipaddress ? ipaddress : ""}?api-key=${ipApiKey}`
+    ).then((data) => {
+        console.log(data);
+        ipData = data;
+
+        if (ipData) {
+            ipaddressElement.innerHTML = ipData.ip;
+            locationElement.innerHTML = `${ipData.location.city ? ipData.location.city : ""}, ${
+        ipData.location.region ? ipData.location.region : ""
+      } ${ipData.location.postalCode ? ipData.location.postalCode : ""}`;
+            timezoneElement.innerHTML = `UTC ${ipData.location.timezone}`;
+            ispElement.innerHTML = ipData.isp;
+
+            // L.latng
+            var lat = ipData.location.lat;
+            var lng = ipData.location.lng;
+            marker.setLatLng([lat, lng]);
+            mymap.setView([lat, lng], 13);
+        }
+    });
+}
+
+function validateIpAddress(ipaddress) {
+    if (/^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|$)){4}$/.test(ipaddress)) {
+        return true;
+    }
+    return false;
+}
+
+searchActionElement.addEventListener("click", (e) => {
+    if (validateIpAddress(searchInput.value)) {
+        setIpData(searchInput.value);
+    }
+});
+
+var mymap = L.map("map").setView([51.505, -0.09], 13);
+
+L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: "mapbox/streets-v11",
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: mapApiKey,
+    }
+).addTo(mymap);
+
+var myIcon = L.icon({
+    iconUrl: "images/icon-location.svg",
+});
+
+var marker = L.marker([51.505, -0.09], { icon: myIcon }).addTo(mymap);
+
+setIpData();
